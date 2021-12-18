191ec85620848ef9cadb125022d141554531339d
Aayush Wadhwani <aayushwadhwani99@gmail.com>
2021-12-18 22:25:38 +0530

    :sparkles: complete basic functionality

--- Birthday-Reminder/src/List.js
+++ Birthday-Reminder/src/List.js
@@ -1,9 +1,25 @@
 import React from 'react';
 
-const List = () => {
+const List = (props) => {
+  let { people } = props;
+  console.log(people);
+  const newPeople = people.map((p)=>{
+    const { name,age,image,id } = p;
+    return (
+      <article className='person' key={id}>
+        <img src={image} alt={name} />
+        <div>
+          <h4>{name}</h4>
+          <p>{age}</p>
+          {/* <a style={{cursor:'pointer', fontWeight:'bolder'}} onClick={() => people = []}>Delete</a> */}
+        </div>
+      </article>
+    )
+  });
+  console.log(newPeople);
   return (
     <>
-      <h2>list component</h2>
+      {newPeople}
     </>
   );
 };
